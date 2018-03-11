cae2c92adc347070941bb0d5f47c7f964b004e6d
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2018-03-11 12:38:21 +0200

    small fixes

--- Seminar/tex/compile.sh
+++ Seminar/tex/compile.sh
@@ -1,5 +1,7 @@
-latex  sig-alternate-sample
-bibtex sig-alternate-sample
-latex  sig-alternate-sample
-latex  sig-alternate-sample
+#latex  sig-alternate-sample
+#bibtex sig-alternate-sample
+#latex  sig-alternate-sample
+#latex  sig-alternate-sample
 
+pdflatex sig-alternate-sample
+bibtex sig-alternate-sample
